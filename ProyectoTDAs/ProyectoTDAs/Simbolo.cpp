8e37fd93828ad9454f4604499c219f1e4db33d5a
antox89 <ator89@outlook.com>
2019-07-30 16:24:24 -0600

    Simbolo

---
+++ ProyectoTDAs/ProyectoTDAs/Simbolo.cpp
@@ -0,0 +1,9 @@
+//
+//  Simbolo.cpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#include "Simbolo.hpp"
---
+++ ProyectoTDAs/ProyectoTDAs/Simbolo.hpp
@@ -0,0 +1,14 @@
+//
+//  Simbolo.hpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#ifndef Simbolo_hpp
+#define Simbolo_hpp
+
+#include <stdio.h>
+
+#endif /* Simbolo_hpp */
