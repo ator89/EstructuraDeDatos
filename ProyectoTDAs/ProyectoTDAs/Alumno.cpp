3b253cd7a2db0746f527437eb56bca25485ec38c
antox89 <ator89@outlook.com>
2019-07-30 16:22:05 -0600

    Alumno

--- ProyectoTDAs/ProyectoTDAs/Alumno.cpp
+++ ProyectoTDAs/ProyectoTDAs/Alumno.cpp
@@ -6,4 +6,42 @@
 //  Copyright © 2019 Angel Torres. All rights reserved.
 //
 
-#include "Alumno.hpp"
+#include "Alumno.h"
+
+//Constructores y destructor
+Alumno::Alumno(){
+    cuenta = "";
+    nombre = "";
+}
+
+Alumno::Alumno(std::string cuenta,std::string nombre){
+    this->cuenta = cuenta;
+    this->nombre = nombre;
+}
+
+Alumno::~Alumno(){}
+
+//Accesores
+std::string Alumno::getCuenta(){
+    return cuenta;
+}
+std::string Alumno::getNombre(){
+    return nombre;
+}
+
+//Mutadores
+void Alumno::setCuenta(std::string cuenta){
+    this->cuenta = cuenta;
+}
+void Alumno::setNombre(std::string nombre){
+    this->nombre = nombre;
+}
+
+//métodos polimórficos
+std::string Alumno::toString(){
+    return "";
+}
+
+bool Alumno::equals(Object* info){
+    return false;
+}
--- ProyectoTDAs/ProyectoTDAs/Alumno.h
+++ ProyectoTDAs/ProyectoTDAs/Alumno.h
@@ -16,11 +16,22 @@ private:
     std::string nombre;
     std::string cuenta;
 public:
-    //Constructores
+    //Constructores y destructor
     Alumno();
     Alumno(std::string,std::string);
     ~Alumno();
     
+    //Accesores
+    std::string getCuenta();
+    std::string getNombre();
+    
+    //Mutadores
+    void setCuenta(std::string);
+    void setNombre(std::string);
+    
+    //métodos polimórficos
+    virtual std::string toString();
+    virtual bool equals(Object*);
 };
 
 #endif
