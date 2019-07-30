1e1b3fbeb00423816af8bd0633501529b3dacd7e
antox89 <ator89@outlook.com>
2019-07-30 17:13:44 -0600

    LinkedList

--- ProyectoTDAs/ProyectoTDAs/LinkedList.h
+++ ProyectoTDAs/ProyectoTDAs/LinkedList.h
@@ -9,10 +9,32 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-#include <stdio.h>
+#include <string>
+#include "TDALista.h"
+#include "Nodo.h"
 
 class LinkedList:public TDALista{
+private:
+    Nodo* head;
+public:
+    //Constructor y destructor
+    LinkedList();
+    ~LinkedList();
     
+    //Métodos administración de la lista
+    virtual void insertar(Object*,int);
+    virtual void imprimir(Object*);
+    virtual bool buscar(Object*);
+    virtual bool borrar(Object*);
+    virtual bool isEmpty();
+    virtual Object* posicion(int);
+    virtual Object* anterior(Object*);
+    virtual Object* siguiente(Object*);
+    virtual bool vaciar(Object*);
+    
+    //Métodos polimórficos heredados de Object
+    virtual std::string toString();
+    virtual bool equals(Object*);
 };
 
 #endif
