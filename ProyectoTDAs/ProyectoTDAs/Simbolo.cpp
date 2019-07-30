a548620ed5b205da2fb0a66d3c3cffbb7c547ebd
antox89 <ator89@outlook.com>
2019-07-30 16:34:07 -0600

    Simbolo

--- ProyectoTDAs/ProyectoTDAs/Simbolo.cpp
+++ ProyectoTDAs/ProyectoTDAs/Simbolo.cpp
@@ -6,4 +6,33 @@
 //  Copyright © 2019 Angel Torres. All rights reserved.
 //
 
-#include "Simbolo.hpp"
+#include "Simbolo.h"
+
+//Constructores y Destructor
+Simbolo::Simbolo(){
+    simbolo = ' ';
+}
+
+Simbolo::Simbolo(char simbolo){
+    this->simbolo = simbolo;
+}
+
+Simbolo::~Simbolo(){}
+
+//Accesor
+char Simbolo::getSimbolo(){
+    return simbolo;
+}
+
+//Mutador
+void Simbolo::setSimbolo(char simbolo){
+    this->simbolo = simbolo;
+}
+
+//Métodos polimórficos
+std::string Simbolo::toString(){
+    return "";
+}
+bool Simbolo::equals(Object* info){
+    return false;
+}
---
+++ ProyectoTDAs/ProyectoTDAs/Simbolo.h
@@ -0,0 +1,35 @@
+//
+//  Simbolo.hpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#ifndef SIMBOLO_H
+#define SIMBOLO_H
+
+#include <string>
+#include "Object.h"
+
+class Simbolo:public Object{
+private:
+    char simbolo;
+public:
+    //Constructores y Destructor
+    Simbolo();
+    Simbolo(char);
+    ~Simbolo();
+    
+    //Accesor
+    char getSimbolo();
+    
+    //Mutador
+    void setSimbolo(char);
+    
+    //Métodos polimórficos
+    virtual std::string toString();
+    virtual bool equals(Object*);
+};
+
+#endif
