5fe69f2fd036dd604e6d7adc1cd36684e1f911c7
antox89 <ator89@outlook.com>
2019-07-30 16:54:04 -0600

    LinkedQueue

---
+++ ProyectoTDAs/ProyectoTDAs/LinkedQueue.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedQueue.cpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#include "LinkedQueue.h"
---
+++ ProyectoTDAs/ProyectoTDAs/LinkedQueue.hpp
@@ -0,0 +1,14 @@
+//
+//  LinkedQueue.hpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#ifndef LINKEDQUEUE_H
+#define LINKEDQUEUE_H
+
+#include <stdio.h>
+
+#endif
