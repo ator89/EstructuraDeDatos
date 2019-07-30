3e83e3722052472deebb1209b7b5717bd3d078f7
antox89 <ator89@outlook.com>
2019-07-30 16:46:08 -0600

    LinkedList

---
+++ ProyectoTDAs/ProyectoTDAs/LinkedList.h
@@ -0,0 +1,18 @@
+//
+//  LinkedList.h
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+
+class LinkedList:public TDALista{
+    
+};
+
+#endif
