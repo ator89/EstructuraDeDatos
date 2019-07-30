c217bac51748b04933ea3be565ea2658c2a42999
antox89 <ator89@outlook.com>
2019-07-30 16:50:18 -0600

    LinkedStack

---
+++ ProyectoTDAs/ProyectoTDAs/LinkedStack.h
@@ -0,0 +1,14 @@
+//
+//  LinkedStack.hpp
+//  ProyectoTDAs
+//
+//  Created by Angel Torres on 7/30/19.
+//  Copyright © 2019 Angel Torres. All rights reserved.
+//
+
+#ifndef LINKEDSTACK_H
+#define LINKEDSTACK_H
+
+#include <stdio.h>
+
+#endif /* LinkedStack_hpp */
