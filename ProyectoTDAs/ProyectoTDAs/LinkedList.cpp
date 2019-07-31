ff9416f45b184dd03f0bbc6362b19810173acfc0
antox89 <ator89@outlook.com>
2019-07-30 18:43:53 -0600

    LinkedList - Insertar

--- ProyectoTDAs/ProyectoTDAs/LinkedList.cpp
+++ ProyectoTDAs/ProyectoTDAs/LinkedList.cpp
@@ -7,3 +7,70 @@
 //
 
 #include "LinkedList.h"
+#include "Alumno.h"
+
+//Constructor y destructor
+LinkedList::LinkedList(){
+    head = NULL;
+}
+LinkedList::~LinkedList(){
+    if(head)
+        delete head;
+}
+
+//Métodos administración de la lista
+void LinkedList::insertar(Object* data,int pos){
+    Nodo* temp1 = new Nodo();
+    temp1->setData(data);
+    temp1->setNext(nullptr);
+    temp1->setPrevious(nullptr);
+    if(head == NULL){
+        return;
+    }
+    else if(pos == 1){
+        temp1->setNext(head);
+        head->setPrevious(temp1);
+        head = temp1;
+        return;
+    }else{
+        for(int i = 0; i < pos-2; i++){
+            temp1 = temp1->getNext();
+        }
+        Nodo* temp2 = temp1->getNext();
+        temp1->setNext(temp2->getNext());
+        temp2->setNext(temp1);
+    }
+}
+
+void LinkedList::imprimir(Object* lista){}
+
+bool LinkedList::buscar(Object* elemento){
+    return false;
+}
+bool LinkedList::borrar(Object*){
+    return false;
+}
+bool LinkedList::isEmpty(){
+    return false;
+}
+Object* LinkedList::posicion(int n){
+    Object* temp = new Alumno();
+    return temp;
+}
+Object* LinkedList::anterior(Object* anterior){
+    return anterior;
+}
+Object* LinkedList::siguiente(Object* siguiente){
+    return siguiente;
+}
+bool LinkedList::vaciar(Object*){
+    return false;
+}
+
+//Métodos polimórficos heredados de Object
+std::string toString(){
+    return "";
+}
+bool equals(Object*){
+    return false;
+}
