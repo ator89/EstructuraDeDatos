a270b9ef616211f4d3f2c1759611b93c6fb7896b
antox89 <ator89@outlook.com>
2019-08-06 05:50:12 -0600

    LinkedStack

--- ProyectoTDAs/ProyectoTDAs/LinkedStack.h
+++ ProyectoTDAs/ProyectoTDAs/LinkedStack.h
@@ -9,6 +9,31 @@
 #ifndef LINKEDSTACK_H
 #define LINKEDSTACK_H
 
-#include <stdio.h>
+#include "TDAPila.h"
+#include "Nodo.h"
+#include <string>
 
-#endif /* LinkedStack_hpp */
+class LinkedStack:public TDAPila{
+private:
+    Nodo* head;
+public:
+    LinkedStack();
+    ~LinkedStack();
+    
+    //insertar en la pila
+    virtual void push(Object*);
+    //ver tope de la pila
+    virtual Object* top();
+    //sacar elemento de la pila
+    virtual Object* pop();
+    //ver si esta vacia
+    virtual bool isEmpty();
+    //eliminar elementos de la pila
+    virtual void anula();
+    
+    //Métodos polimórficos de Object
+    std::string toString();
+    bool equals(Object*);
+};
+
+#endif
