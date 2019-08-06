9ca32781ebdad5e595b62c8579c1c18b68db94a3
antox89 <ator89@outlook.com>
2019-08-06 06:20:48 -0600

    LinkedQueue

--- ProyectoTDAs/ProyectoTDAs/LinkedQueue.cpp
+++ ProyectoTDAs/ProyectoTDAs/LinkedQueue.cpp
@@ -7,3 +7,88 @@
 //
 
 #include "LinkedQueue.h"
+
+
+LinkedQueue::LinkedQueue(){
+    inicio = NULL;
+    final = NULL;
+}
+
+LinkedQueue::~LinkedQueue(){
+    if(final)
+        delete final;
+}
+
+//insertar en la cola
+void LinkedQueue::queue(Object* data){
+    Nodo* newNode = new Nodo();
+    newNode->setData(data);
+    if(inicio == NULL && final==NULL){
+        final = newNode;
+        inicio = newNode;
+    }else{
+        newNode->setNext(final);
+        final->setPrevious(newNode);
+        final = newNode;
+    }
+}
+
+//ver tope de la cola
+Object* LinkedQueue::front(){
+    if(inicio)
+        return inicio->getData();
+    else
+        return NULL;
+}
+
+//sacar elemento de la cola
+Object* LinkedQueue::dequeue(){
+    if(inicio==NULL){
+        return NULL;
+    }
+    else if(inicio==final){
+        Nodo* temp = inicio;
+        Object* tempData = temp->getData();
+        temp->setData(NULL);
+        delete temp;
+        inicio = NULL;
+        final = NULL;
+        return tempData;
+    }else{
+        Nodo* temp= inicio;
+        inicio = temp->getPrevious();
+        Object* tempData = temp->getData();
+        temp->setData(NULL);
+        delete temp;
+        inicio->setNext(NULL);
+        return tempData;
+    }
+}
+
+//ver si la cola está vacia
+bool LinkedQueue::isEmpty(){
+    return inicio == NULL;
+}
+
+//eliminar elementos de la cola
+void LinkedQueue::clear(){
+    if(final)
+        delete final;
+    final = NULL;
+    inicio = NULL;
+}
+
+//Métodos polimórficos heredados de Object
+std::string LinkedQueue::toString(){
+    Nodo* tmp = inicio;
+    std::string s;
+    while(tmp != nullptr){
+        s+=tmp->getData()->toString();
+        tmp = tmp->getNext();
+    }
+    return s;
+}
+
+bool LinkedQueue::equals(Object*){
+    return false;
+}
--- ProyectoTDAs/ProyectoTDAs/LinkedQueue.h
+++ ProyectoTDAs/ProyectoTDAs/LinkedQueue.h
@@ -9,6 +9,32 @@
 #ifndef LINKEDQUEUE_H
 #define LINKEDQUEUE_H
 
-#include <stdio.h>
+#include "Nodo.h"
+#include "TDALista.h"
+
+class LinkedQueue:public TDALista{
+private:
+    Nodo* inicio;
+    Nodo* final;
+    
+public:
+    LinkedQueue();
+    ~LinkedQueue();
+    
+    //insertar en la cola
+    virtual void queue(Object*);
+    //ver tope de la cola
+    virtual Object* front();
+    //sacar elemento de la cola
+    virtual Object* dequeue();
+    //ver si esta vacia
+    virtual bool isEmpty();
+    //eliminar elementos de la cola
+    virtual void clear();
+    
+    //Métodos polimórficos de Object
+    std::string toString();
+    bool equals(Object*);
+};
 
 #endif
