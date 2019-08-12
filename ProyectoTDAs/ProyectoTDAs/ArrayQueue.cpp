841efb9444b21614f099e5af2ce10f86ef61ea30
antox89 <ator89@outlook.com>
2019-08-12 09:28:14 -0600

    ArrayQueue

--- ProyectoTDAs/ProyectoTDAs/ArrayQueue.cpp
+++ ProyectoTDAs/ProyectoTDAs/ArrayQueue.cpp
@@ -2,8 +2,91 @@
 //  ArrayQueue.cpp
 //  ProyectoTDAs
 //
-//  Created by Angel Torres on 7/30/19.
-//  Copyright © 2019 Angel Torres. All rights reserved.
-//
+
 
 #include "ArrayQueue.h"
+
+//Constructor
+ArrayQueue::ArrayQueue()
+{
+    capacity=1024;
+    array=new Object*[capacity];
+    size=0;
+}
+
+ArrayQueue::~ArrayQueue()
+{
+    //liberar memoria de los objetos que están en la cola
+    for(int i =0;i<size;i++){
+        if(array[i]!=NULL){//validar
+            delete array[i];
+            array[i]=NULL;
+        }
+    }
+    delete[] array;
+}
+
+/*retorna si la cola está vacía*/
+bool ArrayQueue::isEmpty(){
+    return size==0;
+}
+
+/*aumenta la capacidad del arreglo*/
+void ArrayQueue::resize(){
+    int newCapacity=capacity*2;//aumentar capacidad
+    Object** newArray=new Object*[newCapacity];//crear nuevo arreglo
+    for(int  i = 0; i<size;i++){
+        newArray[i]=array[i];
+        array[i]=NULL;
+    }
+    delete[] array;//liberar espacio
+    array=newArray;
+    capacity=newCapacity;
+}
+
+/*pone el parámetro en la cola, al inicio del arreglo*/
+void ArrayQueue::queue(Object* data){
+    //revisar que hay espacio para insertar
+    if(size==capacity){
+        resize();//si no hay, expandir el arreglo
+    }
+    
+    //hacer corrimientos para insertar en la casilla 0
+    for(int i = size;i>0;i--){
+        array[i]=array[i-1];
+    }
+    
+    //insertar
+    array[0]=data;
+    //incrementar size
+    size++;
+}
+
+/*retorna el objeto en el frente de la cola*/
+Object* ArrayQueue::front(){
+    if(size!=0)
+        return array[size-1];
+    else
+        return NULL;
+}
+
+/*saca el objeto del frente de la cola*/
+Object* ArrayQueue::deQueue(){
+    if(size>0){
+        Object* tmp=array[size-1];
+        array[size-1]=NULL;
+        size--;
+        return tmp;
+    }else
+        return NULL;
+}
+/*borra los elementos de la cola*/
+void ArrayQueue::clear(){
+    for(int i =0;i<size;i++){
+        if(array[i]!=NULL){//validar
+            delete array[i];
+            array[i]=NULL;
+        }
+    }
+    size=0;
+}
--- ProyectoTDAs/ProyectoTDAs/ArrayQueue.h
+++ ProyectoTDAs/ProyectoTDAs/ArrayQueue.h
@@ -2,13 +2,37 @@
 //  ArrayQueue.hpp
 //  ProyectoTDAs
 //
-//  Created by Angel Torres on 7/30/19.
-//  Copyright © 2019 Angel Torres. All rights reserved.
-//
+
 
 #ifndef ARRAYQUEUE_H
 #define ARRAYQUEUE_H
 
-#include <stdio.h>
+#include "TDACola.h"
+
+class ArrayQueue:public TDACola{
+private:
+    Object** array;//arreglo de apuntadores a object
+    int capacity;//tamaño del arreglo
+    int size;//cantidad de elementos en la cola
+    void resize();/*aumenta la capacidad del arreglo*/
+public:
+    ArrayQueue();
+    ~ArrayQueue();
+    
+    //insertar en la cola
+    virtual void queue(Object*)=0;
+    //ver tope de la cola
+    virtual Object* front()=0;
+    //sacar elemento de la cola
+    virtual Object* deQueue()=0;
+    //ver si esta vacia
+    virtual bool isEmpty()=0;
+    //eliminar elementos de la cola
+    virtual void clear()=0;
+    
+    //Métodos polimórficos heredados de Object
+    std::string toString() = 0;
+    bool equals(Object*) = 0;
+};
 
 #endif
