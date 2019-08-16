d3d1f65648e2884cea44f03cc4816fc0ba076cd9
antox89 <ator89@outlook.com>
2019-08-16 04:59:23 -0600

    TDACola

--- ProyectoTDAs/ProyectoTDAs/TDACola.h
+++ ProyectoTDAs/ProyectoTDAs/TDACola.h
@@ -2,15 +2,11 @@
 //  TDACola.hpp
 //  ProyectoTDAs
 //
-//  Created by Angel Torres on 7/30/19.
-//  Copyright © 2019 Angel Torres. All rights reserved.
-//
 
 #ifndef TDACOLA_H
 #define TDACOLA_H
 
 #include "Object.h"
-#include <string>
 
 class TDACola:public Object{
 public:
@@ -21,15 +17,15 @@ public:
     //ver tope de la cola
     virtual Object* front()=0;
     //sacar elemento de la cola
-    virtual Object* dequeue()=0;
+    virtual Object* deQueue()=0;
     //ver si esta vacia
     virtual bool isEmpty()=0;
     //eliminar elementos de la cola
     virtual void clear()=0;
     
     //Métodos polimórficos heredados de Object
-    std::string toString() = 0;
-    bool equals(Object*) = 0;
+    virtual std::string toString()=0;
+    virtual bool equals(Object*)=0;
 };
 
 #endif
