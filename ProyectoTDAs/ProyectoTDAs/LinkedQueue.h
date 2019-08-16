f1d7730bf8d1e952755b141004d21a0d149a6796
antox89 <ator89@outlook.com>
2019-08-16 05:05:42 -0600

    LinkedQueue.h

--- ProyectoTDAs/ProyectoTDAs/LinkedQueue.h
+++ ProyectoTDAs/ProyectoTDAs/LinkedQueue.h
@@ -2,17 +2,14 @@
 //  LinkedQueue.hpp
 //  ProyectoTDAs
 //
-//  Created by Angel Torres on 7/30/19.
-//  Copyright © 2019 Angel Torres. All rights reserved.
-//
 
 #ifndef LINKEDQUEUE_H
 #define LINKEDQUEUE_H
 
 #include "Nodo.h"
-#include "TDALista.h"
+#include "TDACola.h"
 
-class LinkedQueue:public TDALista{
+class LinkedQueue:public TDACola{
 private:
     Nodo* inicio;
     Nodo* final;
@@ -26,15 +23,15 @@ public:
     //ver tope de la cola
     virtual Object* front();
     //sacar elemento de la cola
-    virtual Object* dequeue();
+    virtual Object* deQueue();
     //ver si esta vacia
     virtual bool isEmpty();
     //eliminar elementos de la cola
     virtual void clear();
     
     //Métodos polimórficos de Object
-    std::string toString();
-    bool equals(Object*);
+    virtual std::string toString();
+    virtual bool equals(Object*);
 };
 
 #endif
